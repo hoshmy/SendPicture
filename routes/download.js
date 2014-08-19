2135af716f8fa0d69063fe798814f18204ae83e6
Itamar Hoshmand <hoshmy@gmail.com>
2014-08-19 20:21:04 +0300

    After first integration upload and download

---
+++ routes/download.js
@@ -0,0 +1,71 @@
+var app = require('../app');
+var fs = require('fs');
+var path = require("path")
+var configuration = require('./configuration');
+var utilities = require('./utilities');
+
+
+var filesDeletionQueue = [];
+try{
+app.get('/download', function(req, res){
+	
+	var directory = path.join(__dirname, '\..\\',configuration.targetImagesDirectory);
+	fs.readdir(directory,function(err, files){
+		if(err)
+		{
+			console.log('Error occured while reading dir:\n' + err);
+			res.writeHead(200, {'Content-Type': 'text/plain' });
+		    res.end('Error reading directory \n');
+		}
+		else
+		{
+			var fileIndex = 0;
+			for(fileIndex=0; fileIndex<files.length;++fileIndex)
+			{
+				if(-1 == filesDeletionQueue.indexOf(files[fileIndex]))
+				{
+					var file = directory + files[fileIndex];
+					console.log('downloading file: ' + file);
+					res.download(file,files[fileIndex]);
+					filesDeletionQueue.push(files[fileIndex]);
+					isAnyFileDownloaded = true;
+					break;
+				}
+			}
+			
+			if(fileIndex === files.length)
+			{/*If no file was downloaded then the connection should be */
+				res.writeHead(200, {'Content-Type': 'text/plain' });
+			    res.end('No Files');
+			}
+
+		}
+    });
+});
+
+app.post('/ackImageDownload',function(req,res,next){
+	var fileForDeletion = req.body;
+	console.log('Asking To Delete file ' + fileForDeletion);
+	
+	var isFileInDeletionQueue = filesDeletionQueue.indexOf(fileForDeletion);
+	if(isFileInDeletionQueue >= 0)
+	{/*the file exists - delete it*/
+		var directory = path.join(__dirname, '\..\\',configuration.targetImagesDirectory);
+		var file = directory + fileForDeletion;
+		fs.unlink(file);
+		filesDeletionQueue.splice(isFileInDeletionQueue,1);
+		res.status(200);
+		res.end();
+	}
+	else
+	{
+		res.status(404);
+		res.end();
+	}
+	
+});
+
+}catch(err)
+{
+	console.log(err);
+}
